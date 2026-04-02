87bbf0c63a03d9550a48d9defab5ce963d73f539
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-04-02 18:29:42 +0700

    feat: implement input function with validation

--- mergesort.cpp
+++ mergesort.cpp
@@ -5,3 +5,30 @@ using namespace std;
 int arr[20], B[20];
 // n is array input size
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n----------------------------" << endl;
+    cout << "\nInputkan Isi elemnt array" << endl;
+    cout << "\n----------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
